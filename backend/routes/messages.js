80bffba16448ef6ef99d8453e13f97080fd22912
GarancePoignart_20250009S <garance.P250009S@sis.hust.edu.vn>
2025-12-02 12:42:28 +0700

    Add GET messages endpoint

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { db } = require("../db");
 
-// code
+// GET /messages (return the list of messages sorted by date)
+router.get("/", async (req, res) => {
+  await db.read(); // read db.json and load datas in db
+  const messages = db.data.messages; // get the list of messages in the database
+  messages.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)); // sort the message from the newer to the older
+  res.json(messages);
+});
 
 module.exports = router;
