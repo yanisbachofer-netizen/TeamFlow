e5e7a2c5012ef02c735ff9e3b38c9dbf67e33903
GarancePoignart_20250009S <garance.P250009S@sis.hust.edu.vn>
2025-12-02 14:04:47 +0700

    update task model to add the users ids tab who can see the task

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,10 +1,11 @@
 class Task {
-  constructor({ id, title, description, status, assignedTo }) {
+  constructor({ id, title, description, status, assignedTo, userAllowedIds = [] }) {
     this.id = id;
     this.title = title;
     this.description = description;
     this.status = status || 'todo';
     this.assignedTo = assignedTo || null;
+    this.userAllowedIds = userAllowedIds; // tab of the users id who can see this task
   }
 }
 module.exports = Task;
